8e3d4275cb652d0775a347cc612cbd7fcbdac9f1
emikridat <64159592+emikridat@users.noreply.github.com>
2020-04-22 18:06:13 +0000

    Add files via upload

---
+++ rar_zip_cracker_aze.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system("sudo apt install figlet")
+os.system("clear")
+os.system("figlet RAR-ZIP CRACKER AZE")
+print("""
+
+1) RAR Şifrə Qırmaq 
+2) ZİP Şifrə Qırmaq
+3) Qırılan Şifrələri Göstər
+
+
+
+""")
+
+secim = input("Seçim : ")
+
+if(secim=="1"):	
+	rar = input("RAR Faylı : ")
+	print("")
+	os.system("sudo rar2john " + rar + " > hash.txt")
+	os.system("sudo john hash.txt --pot=sifre.txt")
+
+
+elif(secim=="2"):	
+	zipdo = input("ZİP Faylı : ")
+	print("")
+	os.system("sudo zip2john " + zipdo + " > hash.txt")
+	os.system("sudo john hash.txt --pot=sifre.txt")
+
+elif(secim=="3"):
+	print("")
+	os.system("sudo cat sifre.txt")
+
+
+
+
